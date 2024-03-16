8d74b1282c49826a98a90cd3af90b9f499645cae
Sebastian Budde <moin@budde.io>
2024-03-16 11:08:01 +0100

    bump version

--- pimp-my-autodarts.user.js
+++ pimp-my-autodarts.user.js
@@ -2,7 +2,7 @@
 // @id           pimp-my-autodarts@https://github.com/sebudde/pimp-my-autodarts
 // @name         Pimp My Autodarts (caller & other stuff)
 // @namespace    https://github.com/sebudde/pimp-my-autodarts
-// @version      0.38
+// @version      0.39
 // @description  Userscript for Autodarts
 // @author       sebudde
 // @match        https://play.autodarts.io/*
